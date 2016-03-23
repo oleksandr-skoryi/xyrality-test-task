fd1d95691645b1925cc7b48ceb0ee263a9427b03
Alex Faster <4190200@gmail.com>
2016-03-23 23:31:53 +0200

    prepare for memory optimisation

--- src/main/java/com/xyrality/model/LotteryEvaluator.java
+++ src/main/java/com/xyrality/model/LotteryEvaluator.java
@@ -1,6 +1,5 @@
 package com.xyrality.model;
 
-import java.util.List;
 
 /**
  * Created by Oleksandr Skoryi on 23.03.16.
@@ -22,15 +21,14 @@ public class LotteryEvaluator {
     }
 
     public int evaluatePlayer(Player player) {
-        List<String> lotteryCombinations = player.getLotteryCombinations();
-        return lotteryCombinations.stream().mapToInt(this::evaluateCombination).sum();
+        return player.getLotteryCombinations().stream().mapToInt(this::evaluateCombination).sum();
     }
 
     private int evaluateCombination(String combination) {
         int[][] matrix = new int[winningCombination.length() + 1][combination.length() + 1];
         for (int x = 1; x < matrix.length; x++) {
             for (int y = 1; y < matrix[x].length; y++) {
-                if (winningCombination.charAt(x-1) == combination.charAt(y-1)) {
+                if (winningCombination.charAt(x - 1) == combination.charAt(y - 1)) {
                     matrix[x][y] = matrix[x - 1][y - 1] + 1;
                 } else {
                     matrix[x][y] = Math.max(matrix[x][y - 1], matrix[x - 1][y]);
@@ -41,8 +39,8 @@ public class LotteryEvaluator {
     }
 
     private static void printMatrix(int[][] matrix) {
-        for (int i=0;i<matrix.length;i++) {
-            for (int j=0;j<matrix[i].length;j++){
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
